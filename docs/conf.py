3ad7609acbee1a6ddd1c341edcc814772e2ad090
Taiga Noumi <Noumi.Taiga@gmail.com>
2019-12-26 23:17:49 +0900

    add type-hint to autodoc

--- docs/conf.py
+++ docs/conf.py
@@ -35,7 +35,8 @@ release = '0.0.1'
 # ones.
 extensions = [
     'sphinx.ext.autodoc',
-    'sphinx.ext.napoleon'
+    'sphinx.ext.napoleon',
+    'sphinx_autodoc_typehints'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
