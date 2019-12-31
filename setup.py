c92beffb008eba490a61acb1b5e9607191babc91
Taiga Noumi <Noumi.Taiga@gmail.com>
2019-12-31 14:47:19 +0900

    revert python support

--- setup.py
+++ setup.py
@@ -49,7 +49,6 @@ setup(
         'License :: OSI Approved :: BSD License',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8'
+        'Programming Language :: Python :: 3.7'
     ], # パッケージ(プロジェクト)の分類。https://pypi.org/classifiers/に掲載されているものを指定可能。
 )
