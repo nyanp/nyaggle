57ed9b42c4ce106c6dbd717b2fc73df1d2139e7c
wakamezake <hotwater1367@gmail.com>
2020-09-20 15:28:20 +0900

    drop python v3.5 support

--- setup.py
+++ setup.py
@@ -55,7 +55,6 @@ setup(
     keywords='nyaggle kaggle',
     classifiers=[
         'License :: OSI Approved :: BSD License',
-        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8'
