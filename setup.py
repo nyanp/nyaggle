902776c777889e50f536815d07651ccb44b8057f
Taiga Noumi <Noumi.Taiga@gmail.com>
2023-07-10 23:24:55 +0900

    #98 drop supporting python 3.6/3.7

--- setup.py
+++ setup.py
@@ -55,8 +55,6 @@ setup(
     keywords='nyaggle kaggle',
     classifiers=[
         'License :: OSI Approved :: BSD License',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8'
     ]
 )
